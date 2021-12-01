050d1d9503aa82188c238f422e2073c8bbf6ff68
Aj <atune@broncos.uncfsu.edu>
2021-12-01 16:53:23 -0500

    Add files via upload

---
+++ Day1/main.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(3000)
+# open our input file and put each line into an array
+input_file = open('input.txt', 'r')
+input_arr=[]
+for n in input_file:
+    input_arr.append(int(n.strip()))
+
+
+def getIncreasing(arr, n=0):
+    if len(arr) == 1:
+        return n
+    else:
+        if arr.pop(0) > arr[0]:
+            return getIncreasing(arr, n)
+        else:
+            return getIncreasing(arr,n+1)
+print(getIncreasing(input_arr))
